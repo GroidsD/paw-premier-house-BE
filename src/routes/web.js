8103d347b8c2ea13e8faf7c6f451774761933cfb
ThanhDuy <duynguuen1206@gmail.com>
2025-10-16 16:37:10 +0700

    update ngrok for deploy backend

--- src/routes/web.js
+++ src/routes/web.js
@@ -10,66 +10,65 @@ const { auditUpload } = require("../middleware/uploadExcel");
 
 let router = express.Router();
 let initWebRoutes = (app) => {
-    //---------------------------------------------------------------------------------------------
+  //---------------------------------------------------------------------------------------------
 
-    // User Routes
-    router.post("/api/login", userController.handleLogin);
-    router.post("/api/logout", userController.handleLogout);
-    // User Change Password (Need Login)
-    router.post(
-        "/api/change-password",
-        authMiddleware,
-        userController.changeMyPassword
-    );
-    // Admin reset Password for user
-    router.post(
-        "/api/reset-password",
-        authMiddleware,
-        adminMiddleware,
-        userController.resetUserPassword
-    );
+  // User Routes
+  router.post("/api/login", userController.handleLogin);
+  router.post("/api/logout", userController.handleLogout);
+  // User Change Password (Need Login)
+  router.post(
+    "/api/change-password",
+    authMiddleware,
+    userController.changeMyPassword
+  );
+  // Admin reset Password for user
+  router.post(
+    "/api/reset-password",
+    authMiddleware,
+    adminMiddleware,
+    userController.resetUserPassword
+  );
 
-    //Get Token
-    router.get("/api/me", authMiddleware, userController.getCurrentUser);
-    //---------------------------------------------------------------------------------------------------------
-    // User Routes
-    router.get(
-        "/api/get-users-role",
-        authMiddleware,
-        userController.getUsersByRole
-    );
-    // Get All Users
-    router.get(
-        "/api/get-all-user",
-        // authMiddleware,
-        // adminMiddleware,
-        userController.getAllUser
-    );
-    //Update User Data
-    router.post(
-        "/api/update-user",
-        authMiddleware,
-        adminMiddleware,
-        userController.updateUserData
-    );
-    //create new user
-    router.post(
-        "/api/create-new-user",
-        authMiddleware,
-        adminMiddleware,
-        userController.createNewUser
-    );
-    //Delete User by ID
-    router.get(
-        "/api/delete-user",
-        // authMiddleware,
-        // adminMiddleware,
-        userController.deleteUserByID
-    );
-    //---------------------------------------------------------------------------------------------
+  //Get Token
+  router.get("/api/me", authMiddleware, userController.getCurrentUser);
+  //---------------------------------------------------------------------------------------------------------
+  // User Routes
+  router.get(
+    "/api/get-users-role",
+    // authMiddleware,
+    userController.getUsersByRole
+  );
+  // Get All Users
+  router.get(
+    "/api/get-all-user",
+    // authMiddleware,
+    // adminMiddleware,
+    userController.getAllUser
+  );
+  //Update User Data
+  router.post(
+    "/api/update-user",
+    // authMiddleware,
+    // adminMiddleware,
+    userController.updateUserData
+  );
+  //create new user
+  router.post(
+    "/api/create-new-user",
+    // authMiddleware,
+    // adminMiddleware,
+    userController.createNewUser
+  );
+  //Delete User by ID
+  router.get(
+    "/api/delete-user",
+    // authMiddleware,
+    // adminMiddleware,
+    userController.deleteUserByID
+  );
+  //---------------------------------------------------------------------------------------------
 
-
-    return app.use("/", router);
+  return app.use("/", router);
 };
 
 module.exports = initWebRoutes;
