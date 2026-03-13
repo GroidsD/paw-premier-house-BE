796ec0602066e7dd6005d19bcf149113b8ff735a
thienvu <vu.vo.cit20@eiu.edu.vn>
2026-03-13 19:00:06 +0700

    style: Refactor serviceRoutes.js for consistent formatting and indentation

--- src/routes/serviceRoutes.js
+++ src/routes/serviceRoutes.js
@@ -5,21 +5,21 @@ import permissionMiddleware from "../middleware/permissionMiddleware.js";
 import rbacMiddleware from "../middleware/rbacMiddleware.js";
 
 import {
-    serviceSingleUpload,
-    handleServiceUploadError,
+  serviceSingleUpload,
+  handleServiceUploadError,
 } from "../middleware/uploadImageServices.js";
 const router = express.Router();
 
 router.post(
-    "/api/service/create",
-    authMiddleware,
-    rbacMiddleware,
-    permissionMiddleware({
-        all: ["dashboard:admin", "service:create"],
-    }),
-    serviceSingleUpload,
-    handleServiceUploadError,
-    serviceController.createService,
+  "/api/service/create",
+  authMiddleware,
+  rbacMiddleware,
+  permissionMiddleware({
+    all: ["dashboard:admin", "service:create"],
+  }),
+  serviceSingleUpload,
+  handleServiceUploadError,
+  serviceController.createService,
 );
 
 router.get("/api/service/get-all", serviceController.getAllServices);
@@ -27,47 +27,47 @@ router.get("/api/service/get-all", serviceController.getAllServices);
 router.get("/api/service/get-by-id", serviceController.getServiceById);
 
 router.get(
-    "/api/service/get-by-category",
-    authMiddleware,
-    rbacMiddleware,
-    permissionMiddleware({
-        all: ["service:read"],
-    }),
-    serviceController.getServicesByCategory,
+  "/api/service/get-by-category",
+  authMiddleware,
+  rbacMiddleware,
+  permissionMiddleware({
+    all: ["service:read"],
+  }),
+  serviceController.getServicesByCategory,
 );
 
 router.put(
-    "/api/service/update",
-    authMiddleware,
-    rbacMiddleware,
-    permissionMiddleware({
-        any: ["dashboard:admin", "dashboard:staff"],
-        all: ["service:update"],
-    }),
-    serviceSingleUpload,
-    handleServiceUploadError,
+  "/api/service/update",
+  authMiddleware,
+  rbacMiddleware,
+  permissionMiddleware({
+    any: ["dashboard:admin", "dashboard:manager", "dashboard:staff"],
+    all: ["service:update"],
+  }),
+  serviceSingleUpload,
+  handleServiceUploadError,
 
-    serviceController.updateService,
+  serviceController.updateService,
 );
 
 router.delete(
-    "/api/service/soft-delete",
-    authMiddleware,
-    rbacMiddleware,
-    permissionMiddleware({
-        all: ["dashboard:admin", "service:delete"],
-    }),
-    serviceController.softDeleteService,
+  "/api/service/soft-delete",
+  authMiddleware,
+  rbacMiddleware,
+  permissionMiddleware({
+    all: ["dashboard:admin", "service:delete"],
+  }),
+  serviceController.softDeleteService,
 );
 
 router.delete(
-    "/api/service/hard-delete",
-    authMiddleware,
-    rbacMiddleware,
-    permissionMiddleware({
-        all: ["dashboard:admin", "service:delete"],
-    }),
-    serviceController.hardDeleteService,
+  "/api/service/hard-delete",
+  authMiddleware,
+  rbacMiddleware,
+  permissionMiddleware({
+    all: ["dashboard:admin", "service:delete"],
+  }),
+  serviceController.hardDeleteService,
 );
 
 export default router;
