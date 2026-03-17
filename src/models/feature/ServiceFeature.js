24b4eb7be1e8011c5a6b8060867880a36758a24e
ThanhDuy <duynguuen1206@gmail.com>
2026-03-17 15:00:32 +0700

    feat: Refactor ServiceFeature model and add seeder for services and features

--- src/models/feature/ServiceFeature.js
+++ src/models/feature/ServiceFeature.js
@@ -1,63 +1,63 @@
-"use strict";
-const { Model } = require("sequelize");
+    "use strict";
+    const { Model } = require("sequelize");
 
-module.exports = (sequelize, DataTypes) => {
-    class ServiceFeature extends Model {
-        static associate(models) {
-            ServiceFeature.belongsTo(models.Service, {
-                foreignKey: "service_id",
-                as: "service",
-            });
+    module.exports = (sequelize, DataTypes) => {
+        class ServiceFeature extends Model {
+            static associate(models) {
+                ServiceFeature.belongsTo(models.Service, {
+                    foreignKey: "service_id",
+                    as: "service",
+                });
 
-            ServiceFeature.belongsTo(models.Feature, {
-                foreignKey: "feature_id",
-                as: "feature",
-            });
+                ServiceFeature.belongsTo(models.Feature, {
+                    foreignKey: "feature_id",
+                    as: "feature",
+                });
+            }
         }
-    }
 
-    ServiceFeature.init(
-        {
-            id: {
-                type: DataTypes.INTEGER,
-                autoIncrement: true,
-                primaryKey: true,
-                allowNull: false,
-            },
-
-            service_id: {
-                type: DataTypes.INTEGER,
-                allowNull: false,
-                references: {
-                    model: "services",
-                    key: "service_id",
+        ServiceFeature.init(
+            {
+                id: {
+                    type: DataTypes.INTEGER,
+                    autoIncrement: true,
+                    primaryKey: true,
+                    allowNull: false,
                 },
-                comment: "Service sử dụng feature",
-            },
 
-            feature_id: {
-                type: DataTypes.INTEGER,
-                allowNull: false,
-                references: {
-                    model: "features",
-                    key: "feature_id",
+                service_id: {
+                    type: DataTypes.INTEGER,
+                    allowNull: false,
+                    references: {
+                        model: "services",
+                        key: "service_id",
+                    },
+                    comment: "Service sử dụng feature",
                 },
-                comment: "Feature được gắn",
-            },
 
-            created_at: {
-                type: DataTypes.DATE,
-                defaultValue: DataTypes.NOW,
-            },
-        },
-        {
-            sequelize,
-            modelName: "ServiceFeature",
-            tableName: "service_features",
-            freezeTableName: true,
-            timestamps: false,
-        },
-    );
+                feature_id: {
+                    type: DataTypes.INTEGER,
+                    allowNull: false,
+                    references: {
+                        model: "features",
+                        key: "feature_id",
+                    },
+                    comment: "Feature được gắn",
+                },
 
-    return ServiceFeature;
-};
+                created_at: {
+                    type: DataTypes.DATE,
+                    defaultValue: DataTypes.NOW,
+                },
+            },
+            {
+                sequelize,
+                modelName: "ServiceFeature",
+                tableName: "service_features",
+                freezeTableName: true,
+                timestamps: false,
+            },
+        );
+
+        return ServiceFeature;
+    };
