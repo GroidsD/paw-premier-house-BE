b57e64e48a3a0a539eb5ec07149734e9568412b4
thienvu <vu.vo.cit20@eiu.edu.vn>
2026-06-10 15:41:17 +0700

    update

--- src/config/firebaseAdmin.js
+++ src/config/firebaseAdmin.js
@@ -1,32 +1,13 @@
 const admin = require("firebase-admin");
-const fs = require("fs");
-const path = require("path");
 
-const serviceAccountPath =
-    process.env.FIREBASE_SERVICE_ACCOUNT_PATH ||
-    path.join(
-        __dirname,
-        "../secrets/pet-sanctuary-7f78f-firebase-adminsdk-fbsvc-83c3e52f53.json",
-    );
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
-if (!fs.existsSync(serviceAccountPath)) {
-    console.error(
-        "❌ Firebase service account file not found:",
-        serviceAccountPath,
-    );
-    process.exit(1);
-}
-
-const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, "utf8"));
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, "\n");
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
 });
-console.log("PRIVATE KEY START:");
-console.log(serviceAccount.private_key.slice(0, 30));
 
-console.log("HAS \\n:", serviceAccount.private_key.includes("\\n"));
-console.log("HAS REAL NEWLINE:", serviceAccount.private_key.includes("\n"));
 console.log("✅ Firebase Admin SDK initialized successfully");
 
 module.exports = admin;
